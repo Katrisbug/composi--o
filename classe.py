a31d791c7caeada4fe801a8dedb4e9c6af81bd47
Katrisbug <bonetkatrina@gmail.com>
2025-09-30 08:24:07 -0300

    def __str__ e gets da classe computador

--- classe.py
+++ classe.py
@@ -9,6 +9,9 @@ class Processador:
     def getVelocidade_ghz(self):
         return self.__velocidade_ghz
     
+    def __str__(self):
+        return f"Processador: {self.__modelo}, {self.__velocidade_ghz} GHz"
+    
 class MemoriaRAM:
     def __init__(self, capacidade_gb:float, tipo:str):
         self.__capacidade_gb = capacidade_gb
@@ -19,6 +22,9 @@ class MemoriaRAM:
     
     def getTipo(self):
         return self.__tipo
+    
+    def __str__(self):
+        return f"Memória RAM: {self.__capacidade_gb} GB, Tipo: {self.__tipo}"
 
 class Armazenamento:
     def __init__(self, tipo:str, capacidade_gb):
@@ -31,6 +37,9 @@ class Armazenamento:
     def getCapacidade_gb(self):
         return self.__capacidade_gb
     
+    def __str__(self):
+        return f"Armazenamento: {self.__tipo}, {self.__capacidade_gb} GB"
+    
 class Computador:
     def __init__ (self, marca:str, modelo:str):
         self.__marca = marca
@@ -38,3 +47,18 @@ class Computador:
         self.__memoria_ram = MemoriaRAM()
         self.__processador = Processador()
         self.__armazenamento = Armazenamento()
+
+    def getMarca(self):
+        return self.__marca
+
+    def getModelo(self):
+        return self.__modelo
+
+    def getProcessador(self):
+        return self.__processador
+
+    def getMemoria_ram(self):
+        return self.__memoria_ram
+
+    def getArmazenamento(self):
+        return self.__armazenamento
