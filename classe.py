a972ef9c302848ed47326ae843376bb66dd87d0c
Katrisbug <bonetkatrina@gmail.com>
2025-09-30 08:02:13 -0300

    classe processador

--- classe.py
+++ classe.py
@@ -1,3 +1,13 @@
+class Processador:
+    def __init__ (self, modelo:str, velocidade_ghz:float):
+        self.__modelo = modelo
+        self.__velocidade_ghz = velocidade_ghz
+    
+    def getModelo(self):
+        return self.__modelo
+    
+    def getVelocidade_ghz(self):
+        return self.__velocidade_ghz
 
 class Computador:
     def __init__ (self, marca:str, modelo:str):
